9f008fbb7b2ff21c16c08991c60d9b96159c6b4b
Delaere, Yvo <ydelaere@vmi-group.com>
2023-08-16 10:34:51 +0200

    Add map from the assignment as an input in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
+#include "heightmap.h"
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello world" << std::endl;
+  std::stringstream sampleMapInput("2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n");
+  HeightMap heightMap = readHeightMap(sampleMapInput);
 }
