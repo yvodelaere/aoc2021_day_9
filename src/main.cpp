dba9bcdfd1bf1095649f9812d87fdf181f1cf3fb
Delaere, Yvo <ydelaere@vmi-group.com>
2023-08-16 10:35:29 +0200

    default return 0

--- src/main.cpp
+++ src/main.cpp
@@ -9,4 +9,5 @@ int main(int argc, char *argv[])
 {
   std::stringstream sampleMapInput("2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n");
   HeightMap heightMap = readHeightMap(sampleMapInput);
+  return 0;
 }
