7350b5481188d4862b9e04d1fe8c25dce5ac86c8
Delaere, Yvo <ydelaere@vmi-group.com>
2023-08-16 12:13:08 +0200

    Added entry test and print which function failed

--- test/heightmap_test.cpp
+++ test/heightmap_test.cpp
@@ -8,19 +8,31 @@ bool dimensionTest(HeightMap aHeightMap)
   bool result = true;
   result &= aHeightMap.size() == 5;
   result &= aHeightMap[0].size() == 10;
+  if (!result)
+  {
+    std::cout << "TEST: " << __FUNCTION__ << " failed" << std::endl;
+  }
+  return result;
+}
+
+bool entryTest(HeightMap aHeightMap)
+{
+  bool result = true;
+  result &= aHeightMap[0][0] == 2;
+  result &= aHeightMap[4][9] == 8;
+  if (!result)
+  {
+    std::cout << "TEST:\t" << __FUNCTION__ << " failed" << std::endl;
+  }
   return result;
 }
 
 int main(void) {
   std::stringstream sampleMapInput("2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n");
   HeightMap heightMap = readHeightMap(sampleMapInput);
-
   bool result = true;
-  // Dimension unit test
+  // Run unit tests
   result &= dimensionTest(heightMap);
-  if (!result)
-  {
-    std::cout << "Dimension test failed" << std::endl;
-  }
+  if (result) { result &= entryTest(heightMap); }
   return !result;
 }
