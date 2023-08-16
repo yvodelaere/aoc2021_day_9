f1fd136456d8c842db2f2b0f6405175df83a6738
Delaere, Yvo <ydelaere@vmi-group.com>
2023-08-16 14:22:10 +0200

    Added opening a file from the argument list and computing the risk level. end of part 1, answer is 514

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,25 @@
 
 int main(int argc, char *argv[])
 {
-  std::stringstream sampleMapInput("2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n");
-  HeightMap heightMap = readHeightMap(sampleMapInput);
+  if (argc != 2)
+  {
+    std::cout << "Invalid number of arguments" << std::endl;
+    return 1;
+  }
+
+  std::cout << "Opening file " << argv[1] << std::endl;
+  std::ifstream input(argv[1]);
+  if (input.is_open())
+  {
+    HeightMap heightMap = readHeightMap(input);
+    int riskLevel = computeRiskLevel(heightMap);
+    std::cout << "Risk level for input map is " << riskLevel << std::endl;
+    input.close();
+  }
+  else
+  {
+    std::cout << "Unable to open file" << std::endl;
+    return 1;
+  }
   return 0;
 }
